f781a8c70dcac645e9fb5f12367de582cd750301
CEC WebTeam <webteam@cheshireeast.gov.uk>
2023-11-09 14:36:33 +0000

    Tweak

--- index.js
+++ index.js
@@ -301,102 +301,128 @@ const site_search = `
   <script>`;
 
 const header = `
-  <div class="visually-hidden-focusable overflow-hidden">
-      <div class="d-print-none"><a tabindex="1" id="skip" class="skip-to-content position-absolute py-1 px-2" style="z-index: 100000;" href="#app">Skip to content</a></div>
+    <div class="visually-hidden-focusable overflow-hidden">
+      <div class="d-print-none">
+        <a
+          tabindex="1"
+          id="skip"
+          class="skip-to-content position-absolute py-1 px-2"
+          style="z-index: 100000"
+          href="#app"
+          >Skip to content</a
+        >
+      </div>
     </div>
     <div id="__ba_panel"></div>
     <header class="cec-header">
       <div class="container">
-        <div class="row">
-          <div class="col-auto">
-            <a
-              class="navbar-brand border border-0 border-secondary"
-              title="Home"
-              href="/home.aspx"
-              >
-              <img
+        <div class="float-start">
+          <a
+            class="navbar-brand border border-0 border-secondary"
+            title="Home"
+            href="/home.aspx"
+          >
+            <img
               title="Home page"
               alt="Cheshire East Council home page"
               height="70"
               width="155"
-              src="/images/non_user/cec-logo-colour-155x70px.png"
-              />
-              <span class="visually-hidden"
-                >Cheshire East Council website home page</span
-              ></a
-            >
-          </div>
-                <div class="col">
-                <div class="col-12 w-100 text-end" style="font-weight: 600">
-                  <div class="ba_listenTranslateToggle d-inline">
-                    <a
-                      class="nav-link pe-0 d-inline ps-0"
-                      onclick="BrowseAloud.panel.toggleBar(); updateToggleText()"
-                      href="#"
-                      >
-                      <i role="img" aria-label="Listen">
-                        <svg
-                          class="d-inline"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="22"
-                          height="22"
-                          viewBox="0 0 24 24"
-                          >
-                          <path
-                          d="M22 0v24l-11-6v-2.278l9 4.909v-17.262l-9 4.91v-2.279l11-6zm-13 6v12h-7v-12h7z"
-                          ></path>
-                          <title>Audio icon</title>
-                        </svg>
-                      </i>
-              <i role="img" aria-label="Translate">
-                <svg
-                  class="d-inline"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  >
-                  <path
-                  d="M17.246 4.042c-3.36 0-3.436-2.895-7.337-2.895-2.108 0-4.075.98-4.909 1.694v-2.841h-2v24h2v-9.073c1.184-.819 2.979-1.681 4.923-1.681 3.684 0 4.201 2.754 7.484 2.754 2.122 0 3.593-1.359 3.593-1.359v-12.028s-1.621 1.429-3.754 1.429zm1.754 9.544c-.4.207-.959.414-1.593.414-.972 0-1.498-.363-2.371-.964-1.096-.755-2.596-1.79-5.113-1.79-1.979 0-3.71.679-4.923 1.339v-7.488c1.019-.902 2.865-1.949 4.909-1.949 1.333 0 1.894.439 2.741 1.103.966.756 2.288 1.792 4.596 1.792.627 0 1.215-.086 1.754-.223v7.766z"
-                  ></path>
-                  <title>Translate icon</title>
-                </svg>
-              </i>
-              <span id="baToggleText" class="d-none d-lg-inline"
-                >Listen and translate</span
-              >
-                    </a>
-                  </div>
-                  <a
-                    class="nav-link d-inline pe-0"
-                    href="/account/cheshire-east-account.aspx"
+              src="https://www.cheshireeast.gov.uk/images/non_user/cec-logo-colour-155x70px.png"
+            />
+            <span class="visually-hidden"
+              >Cheshire East Council website home page</span
+            ></a
+          >
+        </div>
+        <div class="float-end icon-bank">
+          <ul class="mb-2 mb-lg-0 ps-0 ms-auto d-inline list-unstyled">
+            <li class="nav-item d-inline text-end">
+              <div class="ba_listenTranslateToggle d-inline">
+                <a
+                  class="nav-link pe-0 d-inline ps-0"
+                  onclick="BrowseAloud.panel.toggleBar(); updateToggleText()"
+                  href="#"
+                >
+                  <i role="img" aria-label="Listen">
+                    <svg
+                      class="d-inline"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="22"
+                      height="22"
+                      viewBox="0 0 24 24"
                     >
-                    <i role="img" aria-label="Account">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="26"
-                        height="26"
-                        fill="Black"
-                        class="bi bi-person"
-                        viewBox="0 0 16 16"
-                        >
-                        <path
-                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
-                        />
-                        <title>Account icon</title>
-                      </svg>
-                    </i>
-                    <span class="d-none d-lg-inline">My account</span>
-                  </a>
-                </div>
-                <div class=" col-12 w-100 text-end cec-search-site">
-                    <label for="searchTextBox" class="hidden sys_search-label">Search</label>
-                    <input type="text" value="Enter keywords" id="searchTextBox" class="sys_searchbox" >
-                    <input type="button" id="SearchQueryControl_SearchButton" value="Search" class="sys_button sys_searchbutton sys_search-button" />
-                </div>
-                </div>
+                      <path
+                        d="M22 0v24l-11-6v-2.278l9 4.909v-17.262l-9 4.91v-2.279l11-6zm-13 6v12h-7v-12h7z"
+                      ></path>
+                      <title>Audio icon</title>
+                    </svg>
+                  </i>
+                  <i role="img" aria-label="Translate">
+                    <svg
+                      class="d-inline"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        d="M17.246 4.042c-3.36 0-3.436-2.895-7.337-2.895-2.108 0-4.075.98-4.909 1.694v-2.841h-2v24h2v-9.073c1.184-.819 2.979-1.681 4.923-1.681 3.684 0 4.201 2.754 7.484 2.754 2.122 0 3.593-1.359 3.593-1.359v-12.028s-1.621 1.429-3.754 1.429zm1.754 9.544c-.4.207-.959.414-1.593.414-.972 0-1.498-.363-2.371-.964-1.096-.755-2.596-1.79-5.113-1.79-1.979 0-3.71.679-4.923 1.339v-7.488c1.019-.902 2.865-1.949 4.909-1.949 1.333 0 1.894.439 2.741 1.103.966.756 2.288 1.792 4.596 1.792.627 0 1.215-.086 1.754-.223v7.766z"
+                      ></path>
+                      <title>Translate icon</title>
+                    </svg>
+                  </i>
+                  <span id="baToggleText" class="d-none d-lg-inline"
+                    >Listen and translate</span
+                  >
+                </a>
+              </div>
+            </li>
+            <li class="nav-item d-inline text-end">
+              <a
+                class="nav-link d-inline pe-0"
+                href="/account/cheshire-east-account.aspx"
+              >
+                <i role="img" aria-label="Account">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="26"
+                    height="26"
+                    fill="Black"
+                    class="bi bi-person"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
+                    />
+                    <title>Account icon</title>
+                  </svg>
+                </i>
+                <span class="d-none d-lg-inline">My account</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="cec-search-site">
+          <div class="sys_search-query-control">
+            <label for="searchTextBox" class="hidden sys_search-label"
+              >Search</label
+            >
+            <input
+              type="text"
+              value="Enter keywords"
+              id="searchTextBox"
+              class="sys_searchbox"
+            />
+            <input
+              type="button"
+              id="SearchQueryControl_SearchButton"
+              value="Search"
+              class="sys_button sys_searchbutton sys_search-button"
+            />
+          </div>
         </div>
       </div>
+      <div style="clear: both"></div>
     </header>`;
 
 const reachdeck = `
